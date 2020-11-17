a8e5052cb3136faceefa663bbad55490764f1ff1
ot316 <olithompson@rocketmail.com>
2020-11-17 18:09:42 +0000

    remove txt files from repo

---
+++ constants.hpp
@@ -0,0 +1,15 @@
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include"errors.h"
+#include <string>
+
+using namespace std;
+
+const std::string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const unsigned int ALPHABET_LENGTH = ALPHABET.size();
+
+#endif
+
+
